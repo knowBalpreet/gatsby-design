bf6cc25cce386dc2f5ca49986020b18b50762404
Balpreet Singh <knowbalpreet@Balpreets-MacBook-Pro.local>
2019-01-26 17:43:27 +0530

    added images and done changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,14 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Proptotype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
